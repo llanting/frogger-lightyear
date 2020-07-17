bada1281670f9f1e45d4f3b85548c8141fafb696
Lanette Lanting <lanettelanting@gmail.com>
2020-07-17 13:52:37 +0200

    Frogger moves

--- game.js
+++ game.js
@@ -1,8 +1,19 @@
+// function start() {
+//     froggerMovement();
+    
+
+    
+// }
+
+// Draw canvas
 let canvas = document.getElementById('gameCanvas');
 canvas.style.border = '5px solid lightblue';
 
 let ctx = canvas.getContext('2d');
 
+let intervalID = 0;
+
+// Load images
 let bgImg = new Image();
 bgImg.src = 'images/kai-pilger-Ef6iL87-vOA-unsplash.jpg'
 
@@ -15,13 +26,83 @@ homeImg.src = 'images/home.png';
 let frogger = new Image();
 frogger.src = 'images/frogger.png'
 
-function draw() {
-    ctx.drawImage(bgImg, 0, 0, canvas.width, canvas.height);
-    ctx.drawImage(baseImg, 140, 400, 200, 100);
-    ctx.drawImage(homeImg, 0, -40, 500, 150);
-    ctx.drawImage(frogger, canvas.width/2, 460, 35, 35);
+let bluePlanet = new Image();
+bluePlanet.src = 'images/bluePlanet.png'
+
+let yellowPlanet = new Image();
+yellowPlanet.src = 'images/yellowPlanet.png'
+
+let blackHole = new Image();
+blackHole.src = 'images/blackHole.png'
+
+
+//Variables
+let frogX = 208;
+let frogY = 560;
+let froghalfWidth = 35/2;
+let halfWidth = canvas.width/2; 
+
+let isrightArrow = false;
+let isleftArrow = false;
+let isupArrow = false;
+
+    // Press arrowkey
+document.addEventListener('keydown', function(event) {
+    console.log('Key pressed', event);
+        if (event.key === 'ArrowRight') {
+            isrightArrow = true;
+            isleftArrow = false;
+            isupArrow = false;
+        }
+        else if (event.key === 'ArrowLeft') {
+            isleftArrow = true;
+            isrightArrow = false;
+            isupArrow = false;
+        }
+        else if (event.key === 'ArrowUp') {
+            isupArrow = true;
+            isleftArrow = false;
+            isrightArrow = false;
+        }
+})
+        
+// Release arrowkey
+document.addEventListener('keyup', function(event) {
+    isupArrow = false;
+    isleftArrow = false;
+    isrightArrow = false;
+            
+})
+
+function froggerMovement() {
+    if (isrightArrow) {
+        frogX++;
+    }
+    else if (isleftArrow) {
+        frogX -= 1;
+    }
+    else if (isupArrow) {
+        frogY -= 1;
+    }
 }
 
-let intervalId = setInterval(() => {
-    requestAnimationFrame(draw)
-}, 1000)
+// Draw images
+function draw() {
+    ctx.drawImage(bgImg, 0, 0, canvas.width, canvas.height);
+    ctx.drawImage(baseImg, halfWidth - 100, 550, 200, 100);
+    ctx.drawImage(homeImg, -20, -40, 500, 100);
+    ctx.drawImage(bluePlanet, 30, 300 - 40, 80, 80);
+    ctx.drawImage(yellowPlanet, 330, 300 - 40, 80, 80);
+    ctx.drawImage(blackHole, halfWidth - 50, 300 - 40, 100, 80);
+    ctx.drawImage(frogger, frogX, frogY, 35, 35);
+    froggerMovement();
+}
+
+intervalId = setInterval(() => {
+    requestAnimationFrame(draw);
+    
+}, 10)
+
+
+
+
--- main.js
+++ main.js
@@ -1,25 +1,117 @@
+// function main() {
+//     let splashScreen;
+//     let gameScreen;
+//     let gameOverScreen;
 
-// function createSplashscreen() {
-//     document.body.innerHTML = ''
+//     function createSplashscreen() {
+//         splashScreen = buildDom(`
+//             <main>
+//                 <div class="title">
+//                     <h1>Frogger Lightyear</h1>
+//                     <p>Frogger has been on an amazing space-adventure. Reach the top and help him come home safe!</p>
+//                     <div>
+//                         <h3>Keys</h3>
+//                         <ul>
+//                             <li>Arrow Up = Up</li>
+//                             <li>Arrow Left = Left</li>
+//                             <li>Arrow Right = Right</li>
+//                         </ul>
+//                     </div> 
+//                     <button id="start-btn" class ="button">START</button>
+//                 </div>
+//             </main>`);
+
+//         document.body.appendChild(splashScreen);
+
+//         let startButton = splashScreen.querySelector("#start-btn");
+
+//         startButton.addEventListener("click", function() {
+//             startGame();
+//         });
+//     }
+
+//     function removeSplashScreen() {
+//         splashScreen.remove();
+//     }
+
+//     function createGamescreen() {
+//         gameScreen = buildDom(`
+//         <main>
+//             <div class='counter'>
+//                 <span class='score'>Score:</span>
+//                 <span class='score'>Lives:</span>
+//                 <img src="life.png" alt='life' class="lives">
+//                 <img src="life.png" alt='life' class="lives">
+//                 <img src="life.png" alt='life' class="lives">
+//             </div> 
+//             <div>
+//                 <canvas id='gameCanvas' width="450" height="500"></canvas>
+//             </div>
+//         </main>`);
+
+//         document.body.appendChild(gameScreen);
+
+//         return gameScreen;
+    
+//     }
+
+//     function removeGameScreen() {
+//         gameScreen.remove();
+//     }
+
+//     function createGameOverscreen() {
+//         gameOverScreen = buildDom(`
+//         <main>
+//             <div>
+//                 <h3>GAME OVER!</h3>
+//                 <p>Frogger Lightyear floats in space for ever...</p>
+//                 <p>Your scored ${totalScore}</p>
+//             </div> 
+//             <div>
+//                 <button id="reset-btn" class ="button">RESTART</button>
+//             </div>
+//             <div>
+//                 <p>In honor of Frogger</p>
+//                 <p>Lanette &copy 2020</p>
+//             </div>
+//         </main>`);
+
+//         document.body.appendChild(gameOverScreen);
+
+//         let resetButton = gameOverScreen.querySelector("#reset-btn");
+
+//         resetButton.addEventListener("click", function() {
+//             startGame();
+//         });
+
+//     }
+
+//     function removeGameOverScreen() {
+//         gameOverScreen.remove();
+//     }
+
+//     function startGame() {
+//         removeSplashScreen();
+//         removeGameOverScreen();
+
+//         game.gameScreen = createGameScreen();
+
+//         game.start();
+
+//         game.gameOverCallback(gameOver);
+//     }
+
+//     function gameOver() {
+//         removeGameScreen();
+//         createGameOverscreen(this.totalScore);
+//     }
+
+//     createSplashscreen();
+
 // }
 
-// function createGamescreen() {
-//     document.body.innerHTML = ''
+// window.addEventListener("load", "main");
 
-    // <div class='counter'></div>
-    //     <span class='score'>Score:</span>
-    //     <span class='score' >Lives:</span>
-    //     <img src="life.png" alt='life' class="lives">
-    //     <img src="life.png" alt='life' class="lives">
-    //     <img src="life.png" alt='life' class="lives">
-    // </div> 
 
-    // <canvas id='gameCanvas' width="450" height="500"></canvas>
-
-// }
-
-// function createGameOverscreen() {
-//     document.body.innerHTML = ''
-// }
 
 
